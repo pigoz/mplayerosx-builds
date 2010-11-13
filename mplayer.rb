ba854ef10aa564bb6a450fff9b96e008e464abc7
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2010-11-13 12:33:20 +0100

    Initial commit. Added brews for mplayer.git and patched ffmpeg-mt.

---
+++ mplayer.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+puts "yes"
+
+class Mplayer <Formula
+  head 'git://repo.or.cz/mplayer.git', :using => :git
+  homepage 'http://repo.or.cz/w/mplayer.git'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'libass' => :build
+  depends_on File.join(File.dirname(__FILE__),'ffmpeg.rb') => :build
+
+  def install
+    ENV.gcc_4_2
+    ENV['CC'] = ''
+    ENV['LD'] = ''
+    ENV['CFLAGS'] = ''
+    ENV['CXXFLAGS'] = ''
+    
+    args = ["--prefix=#{prefix}", "--cc=gcc-4.2", "--enable-largefiles", 
+            "--disable-x11", "--disable-gl", "--disable-mencoder",
+            "--enable-apple-remote", "--enable-macosx-bundle",
+            "--enable-macosx-finder"]
+    
+    args << "--target=x86_64-Darwin" if snow_leopard_64?
+    
+    system "./configure", *args
+    system "make"
+    system "make install"
+  end
+end
